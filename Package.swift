519d897e2c8afce73721dc6e038821a1cf49a983
OberstKrueger <ok@oberstkrueger.com>
2023-01-15 02:18:33 -0800

    Updated swift-argument-parser to 1.2.1

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "TerminalHelpers",
     platforms: [.macOS(.v13)],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", exact: "1.2.0")
+        .package(url: "https://github.com/apple/swift-argument-parser.git", exact: "1.2.1")
     ],
     targets: [
         .executableTarget(
