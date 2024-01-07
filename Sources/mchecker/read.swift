2ce243b68b2273b5eadf71c789e2e53b5407a530
OberstKrueger <ok@oberstkrueger.com>
2024-01-07 02:56:57 -0800

    Removed genre checking

--- Sources/mchecker/read.swift
+++ Sources/mchecker/read.swift
@@ -5,7 +5,7 @@ func newRead() -> [String: [String: Int]] {
 
     var music: [String: [String: Int]] = [:]
 
-    for song in library.allMediaItems.filter({ $0.mediaKind == .kindSong }).filter({ genreCheck(genre: $0.genre) }) {
+    for song in library.allMediaItems.filter({ $0.mediaKind == .kindSong }) {
         let artist = song.album.albumArtist ?? "Unknown Artist"
         let genre = song.genre
         let year = song.year
@@ -18,24 +18,4 @@ func newRead() -> [String: [String: Int]] {
     }
 
     return music
-}
-
-func genreCheck(genre: String) -> Bool {
-    switch genre {
-    case "Epic Metal": return true
-    case "Industrial": return true
-    case "Metal"     : return true
-    case "Rock"      : return true
-    default          : return false
-//    case "ASMR":       return false
-//    case "Comedy":     return false
-//    case "Film":       return false
-//    case "Gaming":     return false
-//    case "Holiday":    return false
-//    case "Learning":   return false
-//    case "Noise":      return false
-//    case "Soundtrack": return false
-//    case "Theme Park": return false
-//    default:           return true
-    }
 }
