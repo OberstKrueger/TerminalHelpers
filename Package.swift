f8572752d8a16a5f06b9ed1bc874fdcdb9a03bea
OberstKrueger <ok@oberstkrueger.com>
2025-03-06 03:34:23 -0800

    Renamed random to okrn

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     targets: [
         .executableTarget(
-            name: "random",
+            name: "okrn",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser")
             ]
